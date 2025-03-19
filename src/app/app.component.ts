ae34f2af15759d688524ec163aaef7f3c1622bee
Isaias Santos <ifelipes@gmail.com>
2025-03-19 09:24:26 -0300

    User Story: 1001 - Criação de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+import agenda from './agenda.json';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'indexa';
+  filtroPorTexto: string = '';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter((contato) => {
+      var result = this.removerAcentos(contato.nome.toLowerCase()).includes(
+        this.removerAcentos(this.filtroPorTexto.toLowerCase())
+      );
+
+      return result;
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter((contato) => {
+      var result = contato.nome.toLowerCase().startsWith(letra);
+      return result;
+    });
+  }
+
+  removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
 }
